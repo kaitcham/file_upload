dad318427f891f3c34944bfe8f9e176a5b868524
Munezero Christian <chamsf5@aol.com>
2023-04-19 22:10:25 +0200

    Import and export error responses

---
+++ server/errors/_index.js
@@ -0,0 +1,11 @@
+const NotFoundError = require('./notFoundError');
+const BadRequestError = require('./badRequestError');
+const UnauthenticatedError = require('./unauthenticatedError');
+const UnauthorizedError = require('./unauthorizedError');
+
+module.exports = {
+  NotFoundError,
+  BadRequestError,
+  UnauthenticatedError,
+  UnauthorizedError,
+};
